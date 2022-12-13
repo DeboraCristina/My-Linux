429851915cfdaa28eda5874f690fb749685e3dc6
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-12-13 14:19:59 -0300

    update gitignore

--- Bash-Configs/gitignore.sh
+++ Bash-Configs/gitignore.sh
@@ -1,5 +1,14 @@
 gitignore()
 {
-	echo ".gitignore
-$1" > .gitignore
+	if [[ $# > 0 ]]
+	then
+		if [[ ! -a ".gitignore" ]]
+		then
+			echo ".gitignore" > .gitignore
+		fi
+		for arg in $@
+		do
+			echo "$arg" >> .gitignore
+		done
+	fi
 }
