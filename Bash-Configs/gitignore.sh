65cb066b8f6a7181778401a28cff8e67f455d369
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-01-31 20:33:45 -0300

    update gitignore generator

--- Bash-Configs/gitignore.sh
+++ Bash-Configs/gitignore.sh
@@ -5,10 +5,14 @@ gitignore()
 		if [[ ! -a ".gitignore" ]]
 		then
 			echo ".gitignore" > .gitignore
+			echo ".gitignore created!"
 		fi
 		for arg in $@
 		do
 			echo "$arg" >> .gitignore
+			echo "\"$arg\" added to .gitignore"
 		done
+	else
+		echo "please input arguments"
 	fi
 }
