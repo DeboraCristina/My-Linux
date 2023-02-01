e8e23cac7051f47743aee6895369ca8b2aab78ae
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-01-31 22:56:14 -0300

    update len.py programa

--- Bash-Configs/len.py
+++ Bash-Configs/len.py
@@ -1,4 +1,12 @@
 from sys import *
 
-if len(argv) > 1:
-    print("O tamanho de \"{}\" é {}.".format(argv[1], len(argv[1])))
+BRANCO = '\033[1;97m'
+VERDE = '\033[1;92m'
+AMARELO = '\033[1;93m'
+NULO = '\033[0m'
+
+if len(argv) < 2:
+    print(f"{BRANCO}Sem Argumentos{NULO}")
+else:
+    for i in range(1, len(argv)):
+        print(f"{BRANCO}O tamanho de {VERDE}\"{argv[i]}\"{BRANCO} é {AMARELO}{len(argv[i])}.{NULO}")
