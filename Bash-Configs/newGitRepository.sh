6aed7b9ee8d63f7112c74500365fe21d1184174d
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-01-31 22:05:24 -0300

    update repository creator

--- Bash-Configs/newGitRepository.sh
+++ Bash-Configs/newGitRepository.sh
@@ -1,7 +1,21 @@
 newrepo()
 {
-	git init
-	git branch -M main
-	git remote add origin $1
-	git remote
+	if [ -d ".git" ]
+	then
+		echo "Already exist a repository"
+	else
+		git init
+		git branch -M main
+
+		echo -n "remote: "
+		read link_remote
+		if [ -z $link_remote ]
+		then
+			echo "no remote link"
+		else
+			git remote add origin $link_remote
+			git remote
+			echo "remote link added"
+		fi
+	fi
 }
